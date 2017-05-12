7c2be1f75ac92ed4142c9a2b0531501d14745cd4
Bernard Chisumo <bchisumo74@yahoo.com>
2017-05-12 13:36:38 +0100

    Starting Working on this Project

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1">
+    <title>Unique</title>
+    <link rel="icon" href="favicon.png" type="image/png">
+    <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('css/style.css')  }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('css/font-awesome.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('css/animate.css') }}" rel="stylesheet" type="text/css">
+
+    <!--[if lt IE 9]>
+    <script src="{{ url('js/respond-1.1.0.min.js')}}"></script>
+    <script src="{{ url('js/html5shiv.js')}}"></script>
+    <script src="{{ url('js/html5element.js')}}"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+@yield('content')
+
+@include('partials.footer')
+
+<script type="text/javascript" src="{{ url('js/jquery-1.11.0.min.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/jquery-scrolltofixed.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/jquery.nav.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/jquery.easing.1.3.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/jquery.isotope.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/wow.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/custom.js') }}"></script>
+
+</body>
+</html>
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<!--Header_section-->
+<header id="header_wrapper">
+    <div class="container">
+        <div class="header_box">
+            <div class="logo"><a href="#"><img src="img/logo.png" alt="logo"></a></div>
+            @include('partials.nav')
+        </div>
+    </div>
+</header>
+<!--Header_section-->
