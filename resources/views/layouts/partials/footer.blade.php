b2d08411b3b6194b27f4424a34c3f2659437a9f2
Abdullah Waheed <iwadday@gmail.com>
2017-09-18 06:03:44 +0500

    Fixed navigation issues and new layout files are saved separately to avoid issues with previous layouts files.

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer_wrapper">
+    <div class="container">
+        <div class="footer_bottom"><span>Copyright © {{ date('Y') }},  Your Company Inc. </span> </div>
+    </div>
+</footer>
