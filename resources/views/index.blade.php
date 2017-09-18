0b4516851e9b45e6aa87c6993eb9550cd612a399
Abdullah Waheed <iwadday@gmail.com>
2017-09-18 06:04:32 +0500

    New Sections files are devided and saved in landing folder to avoid conflict on pages folder for developer.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,25 @@
+@extends('layouts.master')
+<!--Header_section-->
+@section('content')
+    <!--Hero_Section-->
+    @include('landing.hero')
 
+    <!--Aboutus-->
+    @include('landing.about')
+
+    <!--Service-->
+    @include('landing.service')
+
+    <!--Portfolio -->
+    @include('landing.portfolio')
+
+    <!--client_logos-->
+    @include('landing.client')
+
+    <!--Team-->
+    @include('landing.team')
+
+    <!--Contact-->
+    @include('landing.contact')
+
+@endsection
